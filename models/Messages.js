103902c709293487f7f54f79b5ce6d62430062c5
Pavan Ingalagi <pavan.ingalagi@furation.tech>
2023-11-06 11:58:48 +0530

    feat: message feature completed

--- models/Messages.js
+++ models/Messages.js
@@ -1,13 +1,16 @@
 module.exports = (sequelize, DataTypes) => {
     const Message = sequelize.define('Message', {
         sender: {
-            type: DataTypes.STRING,
+            type: DataTypes.INTEGER,
+            allowNull: false,
         },
         receiver: {
-            type: DataTypes.STRING,
+            type: DataTypes.INTEGER,
+            allowNull: false,
         },
         message: {
             type: DataTypes.TEXT,
+            allowNull: false,
         },
     });
     return Message;
